b3ef2dc42ae6f015e56167d52d30a87ef9a84a94
Priya14 <shanmugapriya141292@gmail.com>
2016-02-28 00:10:48 +0530

    Added Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   def current_cart
@@ -8,5 +9,12 @@ class ApplicationController < ActionController::Base
     cart = Cart.create
     session[:cart_id] = cart.id
     cart
+  end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_in)        << :username
+    devise_parameter_sanitizer.for(:sign_up)        << :username
+    devise_parameter_sanitizer.for(:account_update) << :username
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160227141345) do
+ActiveRecord::Schema.define(version: 20160227180136) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 20160227141345) do
     t.decimal  "price",       precision: 8, scale: 2
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "encrypted_password"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
 end
